f9d3eb382075af436a0f2cc226275c7274281a16
maeld974 <mael.deschamps.pro@gmail.com>
2021-01-27 15:47:04 +0400

    Demo Sprint #1

--- app.py
+++ app.py
@@ -3,56 +3,217 @@ import json
 import os
 
 app = Flask(__name__)
-with open(os.path.join("static", "data.json")) as json_file:
+with open(os.path.join("static", "data2.json")) as json_file:
     data = json.load(json_file)
 
 
 candidates_list = {
 "candidates": [
-    {
-      "id_candidat": "177",
-      "civilite": "M.",
-      "nom": "MAGNIEN",
-      "prenom": "Cyril",
-      "ville": "Villiers sur Loir",
-      "previous_job": "LEGAL|",
-      "salary": 145000,
-      "linkedin": "https://www.linkedin.com/in/cmagnien"
-    },
-    {
-      "id_candidat": "178",
-      "civilite": "Mme.",
-      "nom": "BLENIAT",
-      "prenom": "Christelle",
-      "ville": "RUEIL MALMAISON",
-      "previous_job": "SUPPORT/HELP DESK|~DIRECTEUR ADMINISTRAIF ET FINANCIER - DIRECTION DE PROJET IT",
-      "salary": None,
-      "linkedin": "https://www.linkedin.com/in/christelle-bleniat-36b91619"
-    },
-    {
-      "id_candidat": "179",
-      "civilite": "Mrs.",
-      "nom": "NEUPLANCHE",
-      "prenom": "Laure",
-      "ville": "Paris",
-      "previous_job": "FI13 External Auditor other firms",
-      "salary": None,
-      "linkedin": None
-    },
-    {
-      "id_candidat": "180",
-      "civilite": "Mr.",
-      "nom": "OUNE-BIVE",
-      "prenom": "Renaud",
-      "ville": "Paris",
-      "previous_job": "B22 Exploitation Banque Commercial",
-      "salary": None,
-      "linkedin": None
-    }]
+{
+		"id_candidat" : "46135",
+		"civilite": "Mr.",
+		"nom": "MARTIN",
+		"prenom": "Eric",
+		"ville": "CELON",
+		"previous_job": "BTP11 Sales Engineer|CONSTRUCTION/~EX03 Deputy Managing Director|EXECUTIF/~EX02 CEO SME|EXECUTIF/~IMM081 Development Director|IMM08 Property transaction//~SSM0211 Managing Director",
+		"salary": 0,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "110696",
+		"civilite": "Mr.",
+		"nom": "REINACHTER",
+		"prenom": "Jean-Baptiste",
+		"ville": "PARIS",
+		"previous_job" : "Chef de produit Tronçonneuses - Débroussailleuses - Taille-Haies - Souffleurs pour les marques Husqvarna et Jonsered",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "162642",
+		"civilite": "Mr.",
+		"nom": "TOULIS",
+		"prenom": "Vincent",
+		"ville": "",
+		"previous_job": "",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "974694",
+		"civilite": "Mr.",
+		"nom": "WEISS",
+		"prenom": "Lionel",
+		"ville": "Evian-les-Bains,FI08 Business Controller|FINANCIAL SERVICES/~CORPORATE FINANCE|",
+		"previous_job":"",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1013739",
+		"civilite": "Mr.",
+		"nom": "DRUGMAN",
+		"prenom": "John",
+		"ville": "",
+		"previous_job": "CS041 Training Consultant|CS04 Human Resources Consultancy//~CS041 Training Consultant|CS04 Human Resources Consultancy//",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1047286",
+		"civilite": "Mrs.",
+		"nom": "BYRON",
+		"prenom": "Isabelle",
+		"ville": "",
+		"previous_job": "SALES|",
+		"salary": 0,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "1092260",
+		"civilite": "Mr.",
+		"nom": "GUENIFFEY",
+		"prenom": "Thierry",
+		"ville": "BRIMEUX",
+		"previous_job": "IND21 Industrialization Manager/Engineer|INDUSTRY/~IND17 Maintenance Engineer/Manager|INDUSTRY/~IND02 Production Manager/Director|INDUSTRY/",
+		"salary":0,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "1108933",
+		"civilite": "Mrs.",
+		"nom": "VITREY",
+		"prenom": "Cécile",
+		"ville": "Köln",
+		"previous_job": "DI0111 Web Merchandiser|DI01 Marketing//~RESPONSBALE MARKETING",
+		"salary": 0,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "1156041",
+		"civilite": "Mr.",
+		"nom": "TOURNIER",
+		"prenom": "Matthieu",
+		"ville": "PARIS",
+		"previous_job": "Directeur Juridique Conformité Risques",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1166550",
+		"civilite": "Mrs.",
+		"nom": "DA SILVA",
+		"prenom": "Corinne",
+		"ville": "Joinville le Pont",
+		"previous_job": "Marketing - Communication",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1175411",
+		"civilite": "Mr.",
+		"nom": "GUIDOUX",
+		"prenom": "Franck",
+		"ville": "Toulouse",
+		"previous_job": "MARKETING~M053 Marketing Manager",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1186731",
+		"civilite": "Mrs.",
+		"nom": "VAN EEKERT",
+		"prenom": "Lucille",
+		"ville": "Paris",
+		"previous_job": "MARKETING|",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1222760",
+		"civilite": "Mr.",
+		"nom": "MARIE",
+		"prenom": "Arnaud",
+		"ville": "",
+		"previous_job": "Directeur Développement, Directeur Commercial",
+		"salary": 0,
+		"linkedin": ""
+	},
+	{
+		"id_candidat" : "1275471",
+		"civilite": "Mr.",
+		"nom": "MONCHAMPS",
+		"prenom": "Sébastien",
+		"ville": "",
+		"previous_job":"INFORMATION SYSTEM|",
+		"salary":62000,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "1306893",
+		"civilite": "",
+		"nom": "PATRICK",
+		"prenom": "Faugerolas",
+		"ville": "",
+		"previous_job": "REAL ESTATE|~HEALTHCARE|",
+		"salary": 0,
+		"linkedin": "https://www.linkedin.com/in/faugerolas-patrick-3048a253"
+	},
+	{
+		"id_candidat" : "1310463",
+		"civilite": "Mr.",
+		"nom": "NORCA",
+		"prenom": "Steeve",
+		"ville": "",
+		"previous_job" : "Management Commercial", 
+		"salary":0,
+		"linkedin":""
+	},
+	{
+		"id_candidat" : "1324676",
+		"civilite": "",
+		"nom" : "VAN HOOL",
+		"prenom": "Marie",
+		"ville": "",
+		"previous_job":"MARKETING|~M03 Brand Marketing|MARKETING/",
+		"salary":0,
+		"linkedin": "https://www.linkedin.com/in/marie-van-hool"
+	},
+	{
+		"id_candidat" : "1331220",
+		"civilite": "Mr.",
+		"nom": "COLONIA",
+		"prenom": "Olivier",
+		"ville": "",
+		"previous_job": "REAL ESTATE|",
+		"salary":0,
+		"linkedin":"https://www.linkedin.com/in/oliviercolonia"
+	},
+	{
+		"id_candidat" : "1346932",
+		"civilite": "Mr.",
+		"nom": "JEAN PAUL FOURDRAINE",
+		"prenom": "Frédéric",
+		"ville": "",
+		"previous_job": "SAFETY/SECURITY/GENERAL RESOURCES|",
+		"salary":0,
+		"linkedin": "https://www.linkedin.com/in/fr%C3%A9d%C3%A9ric-jean-paul-fourdraine-a353958a"
+	},
+	{
+		"id_candidat" : "1350082",
+		"civilite": "Mrs.",
+		"nom": "DEFOSSEZ",
+		"prenom": "Anne sophie",
+		"ville": "",
+		"previous_job":"HEALTHCARE|",
+		"salary":0,
+		"linkedin": "https://www.linkedin.com/in/anne-sophie-defossez-a448aa69"
+	}
+]
 }
 
 
-@app.route('/missions/list')
+@app.route('/missions/list', methods=['GET', 'POST', 'OPTIONS'])
 def list_missions():
     id_consultant = request.args.get('id_consultant')
     if id_consultant:
@@ -80,17 +241,18 @@ def list_missions():
             })
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Methods', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
     return response
 
 
-@app.route('/candidates', methods=['GET', 'POST'])
+@app.route('/candidates', methods=['GET', 'POST', 'OPTIONS'])
 def get_candidates():
     id_mission = request.args.get('id_mission')
     if id_mission:
-        if id_mission == "10000":
+        if id_mission == "19129":
             response = jsonify({
                 "success": True,
-                "id_mission": "10000",
+                "id_mission": "19129",
                 "candidates": candidates_list["candidates"],
                 "message": f"List of candidates for id_mission = {id_mission} successfuly returned"
             })
@@ -110,6 +272,7 @@ def get_candidates():
             })
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Methods', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
     return response
 
 
